80497618daffadfebfd130f062b83cf9fea14e0d
Mishi Pan <47800034+Atroposs@users.noreply.github.com>
2026-06-15 01:22:20 -0600

    Update apps.py

--- proyecto_seguro/apps/bienes/apps.py
+++ proyecto_seguro/apps/bienes/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BienesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.bienes"
+class CamionesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.camiones'
+    verbose_name = 'Gestión de Camiones'
